ecef2358aa5b33026f13cdb28f28ad5e6b5d8c2d
Rose Judge <rose.harber@gmail.com>
2017-05-22 14:57:39 -0700

    Prompt the user for a word to determine if the word is a palindrome (first pass)

---
+++ Palindrome.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Palindrome
+{
+
+	public static void main( String[] args ) {
+
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a string of characters with no spaces:");
+		// TO DO -- NOTE: doesn't protect against empty input
+		// Will continue to print blank lines until valid input entered.
+		String input = keyboard.next();
+		if ( isPalindrome(input) )
+			System.out.println("Your string IS a Palindrome");
+		else
+			System.out.println("Your string is NOT a Palindrome");
+
+	}
+
+	public static boolean isPalindrome( String word )
+	{
+		int length = word.length();
+		boolean ans = false;
+
+		// base case
+		if ( length <= 1 )
+			return !ans; //true
+
+		// Don't forget: Strings are 0 indexed. 
+		// We actually want n-1 for the index
+		int i, end;
+		for (i = 0,end = word.length() - 1; i <= end; i++,end--) {
+			if (word.charAt(i) == word.charAt(end))
+				continue;
+			else
+				return ans; // false
+		}
+
+		return !ans; //true
+	
+	}
+}
